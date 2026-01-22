7e0c49b180caffc9de21f27ca8a8c4caf037e17c
BhartiChennan <chennanbharti@gmail.com>
2026-01-22 16:39:25 +0530

    Update SeatRepository.java

--- src/main/java/com/xyz/moviebooking/repository/SeatRepository.java
+++ src/main/java/com/xyz/moviebooking/repository/SeatRepository.java
@@ -4,12 +4,10 @@ import com.xyz.moviebooking.persistence.SeatEntity;
 import java.util.List;
 import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface SeatRepository extends JpaRepository<SeatEntity, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select s from SeatEntity s where s.showtimeId = :showtimeId and s.seatNumber in :seatNumbers")
     List<SeatEntity> findSeatsForUpdate(@Param("showtimeId") String showtimeId,
                                         @Param("seatNumbers") List<String> seatNumbers);
